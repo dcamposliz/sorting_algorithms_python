c2bd5ae2b15901562e91fd7abf8ca8f16bef2b99
dcamposliz <dcampos.liz@gmail.com>
2019-12-24 20:03:19 -0500

    update SelectionSort, still working on it

--- SelectionSort.py
+++ SelectionSort.py
@@ -4,7 +4,10 @@ class SelectionSort:
 
         self.array = array
         self.array_length = len(array)
-        self.compare_position = 0
+        
+        self.left_compare_index = None
+        self.right_compare_index = None
+
         self.sorted_end = 0
     
     def sort(self):
@@ -22,6 +25,18 @@ class SelectionSort:
 
             for i in range(0, self.array_length - 1):
                 
+                for j in range(self.sorted_end, self.array_length - 1):
+                    
+                    self.left_compare_index = j
+                    self.right_compare_index = j + 1
+
+                    if self.array[self.left_compare_index] > self.array[self.right_compare_index]:
+                        print("  {0} is larger than {1}".format(self.array[self.left_compare_index], self.array[self.right_compare_index]))
+
+                self.sorted_end = self.sorted_end + 1
+                print("----- End of iteration {0} -----".format(i))
+
+
     
     def swap(self, left_position, right_position):
         temp_left_position = self.array[left_position]
@@ -29,3 +44,8 @@ class SelectionSort:
 
         self.array[right_position] = temp_left_position
         self.array[left_position] = temp_right_positon
+
+x = [17, 3, 5, 13, 11, 7]
+some_shit = SelectionSort(x)
+
+some_shit.sort()
